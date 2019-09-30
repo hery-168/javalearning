8f1fd271a51a9d35b08bd556219137136d585975
yongheng <2863671681@qq.com>
2019-09-30 10:56:34 +0800

    init

---
+++ src/main/java/com/hery/App.java
@@ -0,0 +1,13 @@
+package com.hery;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
