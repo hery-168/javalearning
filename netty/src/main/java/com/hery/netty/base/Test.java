9fecb5ba4e219dc8e6f1fa224824dca3ec26e97c
hery <hery>
2021-11-27 17:21:50 +0800

    add netty base

---
+++ netty/src/main/java/com/hery/netty/base/Test.java
@@ -0,0 +1,9 @@
+package com.hery.netty.base;
+
+import io.netty.util.NettyRuntime;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(NettyRuntime.availableProcessors());
+    }
+}
