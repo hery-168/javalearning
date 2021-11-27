7b44f98cb07267f996e7580390b6ac19234ae0fc
hery <hery>
2021-11-27 17:39:17 +0800

    add netty http

---
+++ netty/src/main/java/com/hery/netty/http/HttpNettyServer.java
@@ -0,0 +1,31 @@
+package com.hery.netty.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class HttpNettyServer {
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new MyServerInitializer());
+
+            ChannelFuture channelFuture = bootstrap.bind(6668).sync();
+            
+            channelFuture.channel().closeFuture().sync();
+
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
